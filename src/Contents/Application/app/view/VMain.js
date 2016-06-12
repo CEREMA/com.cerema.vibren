76f6d02b6e9736f2fdbfab32338247321267ac30
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-06-12 21:39:27 +0200

    Update# 11

--- src/Contents/Application/app/view/VMain.js
+++ src/Contents/Application/app/view/VMain.js
@@ -7,93 +7,95 @@ App.view.define('VMain', {
 	layout: "border",
 
 	items: [{
-		region: 'north'
-		, height: 25
-		, minHeight: 25
-		, border: false
-		, baseCls: 'cls-header'
-		, xtype: "Menu"
-		, itemId: "MenuPanel"
-		, menu: [{
-			text: "Etudes"
-			, menu: [{
-				text: "Nouveau"
-				, itemId: "MNU_NEW"
-      }, {
-				text: "Ouvrir"
-				, itemId: "MNU_OPEN"
-      }]
-    }]
-  }, {
-		region: "center"
-		, split: true
-		, itemId: "ClearWater"
-		, hidden: false
-		, bodyCls: "wallpaper_rainbow"
-  }, {
-		region: "center"
-		, split: true
-		, hidden: true
-		, itemId: "mainScreen"
-		, bodyStyle: "background-color: white"
-		, layout: "vbox"
-		, items: [{
-			padding: 10
-			, fieldLabel: "Etude"
-			, itemId: "etude"
-			, xtype: "textfield"
-			, width: "100%"
-			, height: 25
-    }, {
+		region: 'north', 
+		height: 25, 
+		minHeight: 25, 
+		border: false, 
+		baseCls: 'cls-header', 
+		xtype: "Menu", 
+		itemId: "MenuPanel", 
+		menu: [{
+			text: "Etudes", 
+			menu: [{
+				text: "Nouveau", 
+				itemId: "MNU_NEW"
+      		}, 
+			{
+				text: "Ouvrir", 
+				itemId: "MNU_OPEN"
+      		}]
+    	}]
+  	}, 
+	{
+		region: "center", 
+		split: true, 
+		itemId: "ClearWater", 
+		hidden: false, 
+		bodyCls: "wallpaper_rainbow"
+	}, 
+	{
+		region: "center", 
+		split: true, 
+		hidden: true, 
+		itemId: "mainScreen", 
+		bodyStyle: "background-color: white", 
+		layout: "vbox", 
+		items: [{
+			padding: 10, 
+			fieldLabel: "Etude", 
+			itemId: "etude", 
+			xtype: "textfield", 
+			width: "100%", 
+			height: 25
+    	}, 
+		{
 			padding: 10
 			, fieldLabel: "Description"
 			, itemId: "description"
 			, xtype: "textarea"
 			, width: "100%"
 		    , height: 55
-    }, {
-			layout: "vbox"
-			, width: "100%"
-			, flex: 1
-			, border: true
-			, padding: 10
-			, items: [
-        // Left menu
+    	}, 
+		{
+			layout: "vbox", 
+			width: "100%", 
+			flex: 1, 
+			border: true, 
+			padding: 10, 
+			items: [
+			{
+				xtype: "grid", 
+				itemId: "acquisitionsGrid", 
+				height: 200, 
+				width: "100%", 
+				selType: 'cellmodel', 
+				tbar: [
 				{
-					xtype: "grid"
-					, itemId: "acquisitionsGrid"
-					, height: 200
-					, width: "100%"
-					, selType: 'cellmodel'
-					, tbar: [
-            // Import bar
-						{
-							text: "Importer"
-							, iconCls: "import"
-							, itemId: "import"
-            }
-          ]
-					, // Acquisitions bar
-					columns: [{
-						text: "Acquisitions"
-						, flex: 1
-						, dataIndex: "label_acquisition"
-          }]
-			, store: App.store.create('App.Acquisitions.getAll', {
-				autoLoad: true
-				, storeId: "gridStore"
-			})
+						text: "Importer", 
+						iconCls: "import", 
+					itemId: "import"
+				}
+				],
+				columns: [{
+					text: "Acquisitions", 
+					flex: 1, 
+					dataIndex: "label_acquisition"
+				}],
+				store: App.store.create('App.Acquisitions.getAll', {
+					autoLoad: true, 
+					storeId: "gridStore"
+				})
         }, 
 		{
-			border: true
-			, flex: 1
-			, tabPosition: "top"
-			, width: "100%"
-			, xtype: 'tabpanel'
-			, itemId: 'chartTab'
-			, defaults: {
-				autoScroll: true
-				, bodyPadding: 10
+			border: true,
+			flex: 1,
+			tabPosition: "top",
+			width: "100%",
+			xtype: 'tabpanel',
+			itemId: 'chartTab',
+			defaults: {
+				autoScroll: true,
+				bodyPadding: 10
 			}
         }
       ]
